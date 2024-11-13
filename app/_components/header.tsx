fee08f571f796775a04cf8c728668dc7fae4db62
System Administrator <root@Silvios-MacBook-Pro.local>
2024-11-13 12:46:13 -0300

    Feat:fisrt commit

---
+++ app/_components/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+    return (
+        <Card>
+            <CardContent className="p-5 flex flex-row items-center justify-between">
+                <Image alt="logo" src="/logo-junior.png" height={5} width={178} />
+                <Button size="icon" variant="outline">
+                    <MenuIcon />
+                </Button>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default Header;
